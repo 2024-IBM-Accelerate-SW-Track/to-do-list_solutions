3717ab1ba20a2614d2af098d62b559079b964741
Marc Velasco <mvelasc@us.ibm.com>
2022-07-07 12:06:21 -0700

    changes for week 5 implementation

--- src/component/SearchTodo.js
+++ src/component/SearchTodo.js
@@ -17,25 +17,8 @@ class SearchTodo extends Component {
   
 
   handleSubmit = (e) => {
-    e.preventDefault();  
-    // HTTP Client to send a GET request
-    Axios({
-      method: "GET",
-      url: "http://localhost:8080/get/searchitem",
-      headers: {
-        "Content-Type": "application/json" 
-      },
-      params: {
-        taskname: this.state.content
-      }
-    }).then(res => {
-      this.setState({
-        tmpdata: JSON.stringify(res.data),
-      });
-      // uncomment to see from the browser console log what is returned 
-      //console.log(this.state.tmpdata);
-    });
-
+    //Begin Here
+    
   };
   
   render() {
