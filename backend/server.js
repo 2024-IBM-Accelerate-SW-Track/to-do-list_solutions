6fa083e18bd9a213bd7667e7a1c8ecb5f610243b
Steven-Whitehead <Steven.Whitehead@ibm.com>
2023-06-15 15:33:47 -0400

    changes from week 4

--- backend/server.js
+++ backend/server.js
@@ -3,53 +3,73 @@ const express = require("express"),
        port = process.env.PORT || 8080,
        cors = require("cors");
 const bodyParser = require('body-parser');
-const fs = require("fs");
+const fs = require("fs").promises;
+const { MongoClient } = require('mongodb');
+
+const useMongo = !!process.env.USE_MONGO
+let collection;
+
+if (useMongo) {
+    console.log("connecting to mongo")
+    const url = 'mongodb://localhost:27017';
+    const client = new MongoClient(url);
+    const dbName = 'to-do-list';
+    client.connect().then(() => {
+        collection = client.db(dbName).collection('documents')
+    })
+}
 
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
+
 app.listen(port, () => console.log("Backend server live on " + port));
 
-app.get("/", (req, res) => {
+app.get("/", (request, ressponse) => {
     res.send({ message: "Connected to Backend server!" });
-    });
+});
 
 //add new item to json file
 app.post("/add/item", addItem)
 
-function addItem (request, response) {
-    // Converting Javascript object (Task Item) to a JSON string
-    let id = request.body.jsonObject.id
-    let task = request.body.jsonObject.task
-    let curDate = request.body.jsonObject.currentDate
-    let dueDate = request.body.jsonObject.dueDate
-    var newTask = {
-      ID: id,
-      Task: task,
-      Current_date: curDate,
-      Due_date: dueDate
-    }
-    const jsonString = JSON.stringify(newTask)
-  
-    var data = fs.readFileSync('database.json');
-    var json = JSON.parse(data);
-    json.push(newTask);
-    fs.writeFile("database.json", JSON.stringify(json), function(err, result) {
-      if (err) { console.log('error', err) }
-      else { console.log('Successfully wrote to file') }
-    });
-    response.send(200)
-    }
+async function addItem (request, response) {
+    try {
+        // Converting Javascript object (Task Item) to a JSON string
+        const id = request.body.jsonObject.id
+        const task = request.body.jsonObject.task
+        const curDate = request.body.jsonObject.currentDate
+        const dueDate = request.body.jsonObject.dueDate
+        const newTask = {
+          ID: id,
+          Task: task,
+          Current_date: curDate,
+          Due_date: dueDate
+        }
 
+        if (useMongo) {
+            await collection.insertMany([newTask]);
+            console.log('Successfully wrote to mongo')
+        } else {
+            const data = await fs.readFile("database.json");
+            const json = JSON.parse(data);
+            json.push(newTask);
+            await fs.writeFile("database.json", JSON.stringify(json))
+            console.log('Successfully wrote to file') 
+        }
+        response.sendStatus(200)
+    } catch (err) {
+        console.log("error: ", err)
+        response.sendStatus(500)
+    }
+}
+
+//** week 6, get all items from the json database*/
 app.get("/get/items", getItems)
-//** week5, get all items from the json database*/
-  function getItems (request, response) {
+async function getItems (request, response) {
     //begin here
-    
-  } 
+};
 
-app.get("/get/searchitem",searchItems)
-//**week 5, search items service */
-  function searchItems (request, response) {
+//** week 6, search items service */
+app.get("/get/searchitem", searchItems) 
+async function searchItems (request, response) {
     //begin here
-    
-  }
+};
